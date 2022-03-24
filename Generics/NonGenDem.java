ad51d0dc429132111a6ab0840d42433b069f2364
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-03-24 18:14:29 +0200

    03.24.22 - Four

--- Generics/NonGenDem.java
+++ Generics/NonGenDem.java
@@ -4,10 +4,17 @@ public class NonGenDem {
 
     public static void main(String...args){
 
-        NonGen iob;
-        iob = new NonGen(99);
+        NonGen iob = new NonGen(99);
         iob.showType();
         int v = (Integer) iob.getOb();
+        System.out.println("Value: " + v);
+
+        NonGen sob = new NonGen("Hello");
+        sob.showType();
+        String str = (String) sob.getOb();
+        System.out.println("Value: " + str);
+        iob = sob;// Type security error
+        v = (Integer) iob.getOb();
         System.out.println("Value: " + v);
 
     }
