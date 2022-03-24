c0d9748bad7c201d346c60a51722381cb2274c86
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-03-24 20:54:46 +0200

    03.24.22 - Nine

---
+++ Generics/practice_03_24_22/MyInterface.java
@@ -0,0 +1,6 @@
+package Generics.practice_03_24_22;
+
+interface MyInterface<T> {
+    double average();
+
+}
--- Generics/practice_03_24_22/Stats.java
+++ Generics/practice_03_24_22/Stats.java
@@ -1,16 +1,20 @@
 package Generics.practice_03_24_22;
 
-public class Stats<T extends Number> {
+class Stats<T extends Number > implements MyInterface {
     T[] nums;
 
     Stats(T[] nums){
 
         this.nums = nums;
 
+
     }
-    double average(){
+
+    public double average(){
         double sum = 0.0;
         for(int i  = 0; i < nums.length;i++) sum += nums[i].doubleValue();
         return sum / nums.length;
+
     }
+
 }
