4c70e5ad255eadf89f5576b2f15183d8dec61300
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-03-24 21:18:09 +0200

    03.24.22 - Ten

--- Generics/practice_03_24_22/Stats.java
+++ Generics/practice_03_24_22/Stats.java
@@ -1,6 +1,6 @@
 package Generics.practice_03_24_22;
 
-class Stats<T extends Number > implements MyInterface {
+class Stats<T extends Number >  {
     T[] nums;
 
     Stats(T[] nums){
@@ -10,11 +10,26 @@ class Stats<T extends Number > implements MyInterface {
 
     }
 
+
     public double average(){
         double sum = 0.0;
         for(int i  = 0; i < nums.length;i++) sum += nums[i].doubleValue();
         return sum / nums.length;
 
     }
+
+    boolean sameAVG(Stats<?> nums){
+        if(average() == nums.average()){
+            return true;
+        }
+        return false;
+    }
+    boolean isAVG(Stats<T> nums){
+        if(average() == nums.average()){
+            return true;
+        }
+        return false;
+    }
+
 
 }
--- Generics/practice_03_24_22/StatsRun.java
+++ Generics/practice_03_24_22/StatsRun.java
@@ -3,14 +3,22 @@ package Generics.practice_03_24_22;
 public class StatsRun {
     public static void main(String...args){
 
-        Integer inums[] = {1,2,3,4,5};
-        Stats<Integer> stats = new Stats<Integer>(inums);
+        Integer[] inums = {1,2,3,4,5};
+        Stats<Integer> iob = new Stats<Integer>(inums);
+        System.out.println("Integer Average: " + iob.average());
 
-        System.out.println("Average: " + stats.average());
-        Double dnums[] = {1.1,2.2,3.3,4.4,5.5};
+        Double[] dnums = {1.1,2.2,3.3,4.4,5.5};
         Stats<Double> dob = new Stats<Double>(dnums);
-        System.out.println("Average: " + stats.average());
+        System.out.println("Double Average: " + dob.average());
 
+        Float[] fnums = {1.0F, 2.0F, 3.0F, 4.0F, 5.0F};
+        Stats<Float> fob = new Stats<Float>(fnums);
+        System.out.println("Float Average: " + fob.average());
 
+        if(iob.sameAVG(dob)){
+            System.out.println("YES");
+        }else {
+            System.out.println("NO");
+        }
     }
 }
