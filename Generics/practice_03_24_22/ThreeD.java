9e01e8a9a5338f744dcb22909280f8779e0ad4d9
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-03-24 21:26:59 +0200

    3D

---
+++ Generics/practice_03_24_22/ThreeD.java
@@ -0,0 +1,10 @@
+package Generics.practice_03_24_22;
+
+public class ThreeD extends TwoD {
+    int z;
+
+    ThreeD(int x,int y, int z){
+        super(x,y);
+        this.z = z;
+    }
+}
--- Generics/practice_03_24_22/TwoD.java
+++ Generics/practice_03_24_22/TwoD.java
@@ -8,5 +8,5 @@ public class TwoD {
         this.x = x;
         this.y = y;
     }
-    
+
 }
