6ee79c0459c4b50e4c2303a57acb109eac813d70
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-03-27 23:24:14 +0300

    3D class changed

--- Generics/practice_03_27_22/THREEd.java
+++ Generics/practice_03_27_22/THREEd.java
@@ -2,5 +2,10 @@ package Generics.practice_03_27_22;
 
 public class THREEd extends TWOd {
     int x,y,z;
-    THREEd(int x,)
+
+    THREEd(int x,int y, int z){
+        super(x,y);
+        this.z = z;
+    }
+    
 }
