448104aaedf347ce21e66548599529e31671f027
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-03-27 23:00:17 +0300

    Four class

---
+++ Generics/practice_03_27_22/Four.java
@@ -0,0 +1,8 @@
+package Generics.practice_03_27_22;
+
+public class Four<T extends String> {
+
+    public void message(T t ){
+
+    }
+}
--- Generics/practice_03_27_22/THR.java
+++ Generics/practice_03_27_22/THR.java
@@ -3,6 +3,7 @@ package Generics.practice_03_27_22;
 public class THR<T extends Number> {
 
     T[] arr_one;
+
     THR(T[] arr_one){
 
         this.arr_one  = arr_one;
