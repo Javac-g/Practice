2c221b337f597f9ab7559dabfaf465d6b13f54e9
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-03-27 23:23:07 +0300

    3D class

---
+++ Generics/practice_03_27_22/THREEd.java
@@ -0,0 +1,6 @@
+package Generics.practice_03_27_22;
+
+public class THREEd extends TWOd {
+    int x,y,z;
+    THREEd(int x,)
+}
--- Generics/practice_03_27_22/TWOd.java
+++ Generics/practice_03_27_22/TWOd.java
@@ -1,7 +1,7 @@
 package Generics.practice_03_27_22;
 
 public class TWOd {
-    
+
     int x,y;
 
     TWOd(int x , int y){
