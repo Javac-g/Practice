56be8175daf650619eed991fa9937da9f8b995c6
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-03-27 22:50:43 +0300

    Meta changed

--- Generics/practice_03_27_22/THR.java
+++ Generics/practice_03_27_22/THR.java
@@ -11,6 +11,7 @@ public class THR<T extends Number> {
 
 
     public void diff(THR<?> arr_two){
-
+        System.out.println("arr one: " + arr_one.getClass().getName());
+        System.out.println("arr two: " + arr_two.getClass().getName());
     }
 }
