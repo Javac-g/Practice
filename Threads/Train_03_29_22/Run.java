f5cad9fdcd0a6a54c347e6141a8a5a0bf12b2976
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-03-29 21:28:43 +0300

    Run

--- Threads/Train_03_29_22/Run.java
+++ Threads/Train_03_29_22/Run.java
@@ -6,18 +6,27 @@ public class Run {
 
         FirstType one = new FirstType("First");
         Second two  = new Second("Second");
+        Three tree = new Three("Third");
         one.start();
         System.out.println(one.name + " is alive: " + one.t.isAlive());
         System.out.println(two.name + " is alive: " + two.t.isAlive());
+        System.out.println(tree.name + " is alive: " + tree.isAlive());
 
         try{
             one.t.join();
             two.t.join();
+            tree.join();
+
         } catch (InterruptedException e) {
+
             e.printStackTrace();
+
         }finally {
+
             System.out.println(one.name + " is alive: " + one.t.isAlive());
             System.out.println(two.name + " is alive: " + two.t.isAlive());
+            System.out.println(tree.name + " is alive: " + tree.isAlive());
+
         }
 
     }
