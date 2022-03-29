014027eb3fa4ef7cc3da5de5579dcd5e627d1b9e
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-03-29 21:35:36 +0300

    Caller

---
+++ Threads/Train_03_29_22/Caller.java
@@ -0,0 +1,21 @@
+package Threads.Train_03_29_22;
+
+public class Caller implements Runnable {
+    String msg;
+    call ob;
+    Thread t;
+
+    Caller(call ob, String msg){
+        this.ob = ob;
+        this.msg = msg;
+        t = new Thread(this);
+        t.start();
+    }
+
+    public void run(){
+
+            ob.calling(msg);
+
+    }
+
+}
