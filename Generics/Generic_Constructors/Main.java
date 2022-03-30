8653df0c102610b3f3a62876b2e22d9a4a6de396
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-03-30 20:03:32 +0300

    Main

--- Generics/Generic_Constructors/Main.java
+++ Generics/Generic_Constructors/Main.java
@@ -1,4 +1,15 @@
 package Generics.Generic_Constructors;
 
 public class Main {
+
+    public static void main(String...args){
+        One one = new One(123);
+        One two = new One(22.4);
+        One three = new One(1.5F);
+
+        one.show();
+        two.show();
+        three.show();
+
+    }
 }
