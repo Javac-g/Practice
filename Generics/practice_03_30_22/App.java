61de9ef7d2e5fa3cd54b87075a362e93494a27dc
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-03-30 19:00:42 +0300

    App

---
+++ Generics/practice_03_30_22/App.java
@@ -0,0 +1,74 @@
+package Generics.practice_03_30_22;
+
+public class App {
+
+    static void show_XY(Map<? extends XY> object){
+
+        for (int i = 0; i < object.coordinates.length; i++){
+
+            System.out.print(object.coordinates[i].x + " " );
+
+            System.out.print(object.coordinates[i].y + " ");
+        }
+        System.out.println("X Y coordinates");
+    }
+
+    static void show_XYZ(Map<? extends XYZ> object){
+
+        for (int i = 0; i <object.coordinates.length; i++ ){
+            System.out.print(object.coordinates[i].x + " ");
+
+            System.out.println(object.coordinates[i].y + " ");
+
+            System.out.println(object.coordinates[i].z + " ");
+        }
+    }
+
+    static void show_XYZT(Map<? extends XYZT> object){
+        for (int i = 0 ; i < object.coordinates.length;i++){
+
+            System.out.print(object.coordinates[i].x + " ");
+
+            System.out.println(object.coordinates[i].y + " ");
+
+            System.out.println(object.coordinates[i].z + " ");
+
+            System.out.println(object.coordinates[i].t + " ");
+        }
+    }
+
+    public static void main(String...args){
+        XY[] two = {
+                new XY(1,1),
+                new XY(2,2),
+                new XY(3,3)
+        } ;
+
+        XYZ[] three = {
+                new XYZ(1,1,1),
+                new XYZ(2,2,2),
+                new XYZ(3,3,3)
+        };
+
+        XYZT[] four = {
+
+                new XYZT(1,1,1,1),
+                new XYZT(2,2,2,2),
+                new XYZT(3,3,3,3)
+        };
+
+        // place our arrays with objects in object
+
+        Map<XY> twoD = new Map<XY>(two);
+        Map<XYZ>threeD = new Map<XYZ>(three);
+        Map<XYZT>fourD = new Map<XYZT>(four);
+
+        //applying our methods
+
+        show_XY(twoD);
+        show_XYZ(threeD);
+        show_XYZT(fourD);
+
+
+    }
+}
