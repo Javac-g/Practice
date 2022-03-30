9f4f490d5ae2659e0484584e9f6829cc450b05bb
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-03-30 18:24:14 +0300

    4D

---
+++ Generics/practice_03_30_22/XYZT.java
@@ -0,0 +1,13 @@
+package Generics.practice_03_30_22;
+
+public class XYZT extends XYZ {
+    int t;
+
+    XYZT(int x, int y ,int z,int t){
+
+        super(x,y,z);
+
+        this.t = t;
+
+    }
+}
