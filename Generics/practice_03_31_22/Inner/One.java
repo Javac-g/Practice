2727c631873b558693d85e5629be1f8046ff5381
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-01 00:54:48 +0300

    Inner

---
+++ Generics/practice_03_31_22/Inner/One.java
@@ -0,0 +1,16 @@
+package Generics.practice_03_31_22.Inner;
+
+public class One {
+    int num;
+
+    One(int num){
+
+        this.num = num;
+
+    }
+    int getNum(){
+
+        return num;
+
+    }
+}
