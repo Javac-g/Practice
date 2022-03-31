6ea58f9c83178a49fe7d6bc1ec35fc8ed28f0d45
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-03-31 22:22:27 +0300

    Interface 2

---
+++ Generics/practice_03_31_22/Interface/App.java
@@ -0,0 +1,4 @@
+package Generics.practice_03_31_22.Interface;
+
+public class App {
+}
---
+++ Generics/practice_03_31_22/Interface/Main.java
@@ -0,0 +1,13 @@
+package Generics.practice_03_31_22.Interface;
+
+public class Main<T> implements MyTools<T> {
+    @Override
+    public T min() {
+        return null;
+    }
+
+    @Override
+    public T max() {
+        return null;
+    }
+}
