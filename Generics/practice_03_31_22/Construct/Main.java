537ff54f96a4c0655e472c66a365d60b463a70ca
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-03-31 21:52:31 +0300

    Constr

---
+++ Generics/practice_03_31_22/Construct/Main.java
@@ -0,0 +1,9 @@
+package Generics.practice_03_31_22.Construct;
+
+public class Main {
+    public static void main(String...args){
+
+
+
+    }
+}
---
+++ Generics/practice_03_31_22/Construct/One.java
@@ -0,0 +1,12 @@
+package Generics.practice_03_31_22.Construct;
+
+public class One {
+
+    <T extends Number>One(T ob){
+
+    }
+
+    <P extends Object,V extends Number>One(P p, V v){
+
+    }
+}
