c409e8a72d2590bef19303c06135e4df480f831d
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-03-31 22:38:17 +0300

    Tools

--- Generics/practice_03_31_22/Interface/App.java
+++ Generics/practice_03_31_22/Interface/App.java
@@ -2,9 +2,12 @@ package Generics.practice_03_31_22.Interface;
 
 public class App {
     public static void main(String...args){
-        Integer[] inums = {3,6,2,8,6};
-        Main<Integer>
 
+        Integer[] inums = {3,6,2,8,6};
+
+        Main<Integer> ini = new Main<>(inums);
+        System.out.println("Min: " + ini.min());
+        System.out.println("Max: " + ini.max());
 
 
     }
