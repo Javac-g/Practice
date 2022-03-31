53cae3fb771199b08b012c48d9885926c99e0c2a
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-03-31 21:41:37 +0300

    Metod

--- Generics/practice_03_31_22/Metods/Main.java
+++ Generics/practice_03_31_22/Metods/Main.java
@@ -1,4 +1,15 @@
 package Generics.practice_03_31_22.Metods;
 
 public class Main {
+
+    public static void main(String...args){
+
+        One one = new One();
+
+        one.call("Hello");
+        one.call(12345);
+
+
+
+    }
 }
