9cad680d30505281efea803b13344e27e7778af0
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-03-31 20:39:48 +0300

    4D

---
+++ Generics/practice_03_31_22/four.java
@@ -0,0 +1,12 @@
+package Generics.practice_03_31_22;
+
+public class four extends three{
+    int t;
+
+    four(int x,int y, int z, int t){
+
+        super(x,y,z);
+        this.t = t;
+
+    }
+}
