4e5d4065c147a327132f20c8c299338f9f13b2c4
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-01 00:06:50 +0300

    Generic hierarchy

---
+++ Generics/practice_03_31_22/Generic_Super/One.java
@@ -0,0 +1,21 @@
+package Generics.practice_03_31_22.Generic_Super;
+
+public class One<T> {
+
+    T ob;
+
+    One(T ob){
+
+        this.ob = ob;
+
+    }
+
+    T getOb(){
+
+        return ob;
+
+    }
+
+
+
+}
