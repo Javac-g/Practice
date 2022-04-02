88708b97f153445434598afdd836c2c5d156aadf
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-02 22:28:23 +0300

    Lambdas with 2 args

--- Lambdas/One.java
+++ Lambdas/One.java
@@ -4,11 +4,14 @@ public class One {
     public static void main(String...args){
         Tool_One toolOne;
         Tool_Two tool_two;
+        Tool_Three tool_three;
         toolOne = () -> 123.45;
         System.out.println(toolOne.getValue());
         toolOne = () -> Math.random() * 100;
         System.out.println(toolOne.getValue());
         tool_two = n -> n * n > 5;
         System.out.println(tool_two.test(5));
+        tool_three  = (n,v) -> (n%v) == 0;
+        System.out.println(tool_three.test(294,5));
     }
 }
