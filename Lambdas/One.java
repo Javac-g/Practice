2d93e27c35dbab5d388b57d77be3a4e618d5655e
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-02 21:52:36 +0300

    Lambdas with args

--- Lambdas/One.java
+++ Lambdas/One.java
@@ -3,9 +3,12 @@ package Lambdas;
 public class One {
     public static void main(String...args){
         Tool_One toolOne;
+        Tool_Two tool_two;
         toolOne = () -> 123.45;
         System.out.println(toolOne.getValue());
         toolOne = () -> Math.random() * 100;
         System.out.println(toolOne.getValue());
+        tool_two = (n) -> n * n > 5;
+        System.out.println(tool_two.test(5));
     }
 }
