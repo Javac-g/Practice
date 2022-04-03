be376278b12bfc1a6fbb75636724ee1dbee385e7
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-03 17:03:49 +0300

    lambda in param

---
+++ Lambdas/LambdasArgument.java
@@ -0,0 +1,30 @@
+package Lambdas;
+
+public class LambdasArgument {
+
+    static String word(Tool_Seven seven,String message){
+
+       return seven.funct(message);
+
+    }
+
+    public static void main(String...args){
+        Tool_Seven seven = (message) -> message.toUpperCase();
+        Tool_Seven revers = (message) ->{
+            String res = "";
+          for(int i = message.length()-1;i >= 0; i --){
+              res = res+message.charAt(i);
+          }
+          return res;
+        };
+
+        String input = "hello its string in upper case";
+        String output = word(seven,input) ;
+        System.out.println(word(revers,input));
+
+        System.out.println(output);
+
+
+
+    }
+}
---
+++ Lambdas/Tool_Seven.java
@@ -0,0 +1,5 @@
+package Lambdas;
+
+public interface Tool_Seven {
+    String funct(String message);
+}
