4d094c8d88e501d22da061303516b17226ea5f3e
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-03 18:23:45 +0300

    lambda in param with generic method train

--- Lambdas/practice_04_03_22/Ver/Main.java
+++ Lambdas/practice_04_03_22/Ver/Main.java
@@ -1,13 +1,18 @@
 package Lambdas.practice_04_03_22.Ver;
 
 public class Main {
+    static <T extends Tool,V> String create(T t, V v){
+
+        return" Result: " + t.func(v) ;
+
+    }
     public static void main(String...args){
 
-       Tool<Integer> one = (num) -> num /3;
+       Tool<Integer> one = (num) -> (num * 11) / 3;
        Tool<String> two = (msg) -> msg.toUpperCase();
 
-        System.out.println(one.func(101));
-        System.out.println(two.func("a b c"));
+        System.out.println(create(one,331));
+        System.out.println(create(two,"a b c d f g"));
 
     }
 }
