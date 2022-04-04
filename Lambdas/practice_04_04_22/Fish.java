18ec4b840aa2de66c162d8b6eae88e3058356cb0
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-04 16:55:11 +0300

    04_04_22 Exc

--- Lambdas/practice_04_04_22/Fish.java
+++ Lambdas/practice_04_04_22/Fish.java
@@ -1,5 +1,5 @@
 package Lambdas.practice_04_04_22;
 
 public interface Fish {
-    String message(String msg);
+    String message(String msg) throws MyEx;
 }
--- Lambdas/practice_04_04_22/Main.java
+++ Lambdas/practice_04_04_22/Main.java
@@ -6,14 +6,20 @@ public class Main {
     static <T extends Tool,V>String funct(T t, V v){
         return "Value: " + t.runner(v);
     };
-    public static void main(String...args){
+    public static void main(String...args) throws MyEx {
         Tool<Integer> one = (numb)-> numb /2;
         Tool<String> two = (msg) -> msg.toUpperCase();
-        Fish curs = (msg)->msg.toUpperCase();
+        Fish curs = (msg)->{
+
+            if(msg.length() >= 16){
+                throw new MyEx("To long message");
+            }
+            return msg.toUpperCase();
+        };
         System.out.println(funct(one,1234));
         System.out.println(funct(two,"Hello"));
         System.out.println(two.runner("runner"));
-        System.out.println(curs.message("hello"));
+        System.out.println(curs.message("asdfgfdsasdfrfdsa"));
 
 
     }
