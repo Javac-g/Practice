2790187ba766c9af5ff893a88a40abad1073d949
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-04 17:16:23 +0300

    Var cach done

---
+++ Lambdas/practice_04_04_22/Variable_Capch/Capt.java
@@ -0,0 +1,19 @@
+package Lambdas.practice_04_04_22.Variable_Capch;
+
+public class Capt {
+
+    public static void main(String...args){
+
+
+        int num = 10;
+        MyFunc one = (n) ->{
+
+            int v = num + n;
+
+            return v;
+        };
+
+
+
+    }
+}
--- Lambdas/practice_04_04_22/Variable_Capch/MyFunc.java
+++ Lambdas/practice_04_04_22/Variable_Capch/MyFunc.java
@@ -1,5 +1,5 @@
 package Lambdas.practice_04_04_22.Variable_Capch;
 
 public interface MyFunc {
-    int func(int n);
+    String func(String n);
 }
