d223deb97e76f9f09295e11c661f393bc2416b58
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-04 16:40:40 +0300

    04_04_22 Param

--- Lambdas/practice_04_04_22/Main.java
+++ Lambdas/practice_04_04_22/Main.java
@@ -1,4 +1,18 @@
 package Lambdas.practice_04_04_22;
 
 public class Main {
+
+
+    static <T extends Tool,V>String funct(T t, V v){
+        return "Value: " + t.runner(v);
+    };
+    public static void main(String...args){
+        Tool<Integer> one = (numb)-> numb /2;
+        Tool<String> two = (msg) -> msg.toUpperCase();
+
+        System.out.println(funct(one,1234));
+        System.out.println(funct(two,"Hello"));
+
+
+    }
 }
--- Lambdas/practice_04_04_22/Tool.java
+++ Lambdas/practice_04_04_22/Tool.java
@@ -1,4 +1,8 @@
 package Lambdas.practice_04_04_22;
 
-public interface Tool<> {
+public interface Tool<T> {
+
+    T runner(T t);
+
+
 }
