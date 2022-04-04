19b2fe4691ef33aba08766538e0f4d013537a71c
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-04 16:46:49 +0300

    04_04_22 Tool Interface 2

--- Lambdas/practice_04_04_22/Main.java
+++ Lambdas/practice_04_04_22/Main.java
@@ -9,9 +9,11 @@ public class Main {
     public static void main(String...args){
         Tool<Integer> one = (numb)-> numb /2;
         Tool<String> two = (msg) -> msg.toUpperCase();
-
+        Fish curs = (msg)->msg.toUpperCase();
         System.out.println(funct(one,1234));
         System.out.println(funct(two,"Hello"));
+        System.out.println(two.runner("runner"));
+        System.out.println(curs.message("hello"));
 
 
     }
