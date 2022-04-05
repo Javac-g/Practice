ba9625db05379e32fda4d3a38d6f7853ac357687
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-05 22:57:05 +0300

    six

---
+++ Lambdas/General_Practice_05_04_22/Five/On.java
@@ -0,0 +1,4 @@
+package Lambdas.General_Practice_05_04_22.Five;
+
+public class On {
+}
---
+++ Lambdas/General_Practice_05_04_22/Five/Tools.java
@@ -0,0 +1,5 @@
+package Lambdas.General_Practice_05_04_22.Five;
+
+public interface Tools {
+    String message(String msg);
+}
