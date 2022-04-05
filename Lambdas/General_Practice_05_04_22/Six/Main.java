164936a59fc5976a83d5e07ee484aee1d90fa74c
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-05 23:30:40 +0300

    Train 2

--- Lambdas/General_Practice_05_04_22/Six/Main.java
+++ Lambdas/General_Practice_05_04_22/Six/Main.java
@@ -1,4 +1,22 @@
 package Lambdas.General_Practice_05_04_22.Six;
 
+import Lambdas.practice_04_03_22.Ver.Tool;
+
 public class Main {
+
+    static String runner(Stool<String> stool,String msg){
+        return stool.function(msg);
+    }
+    static <T> String run(Stool<T> ftool, T msg){
+        return ""+ ftool.function(msg);
+    }
+    public static void main(String...args){
+
+        Stool<String> one = msg->msg.toLowerCase();
+        System.out.println(run(Metod::Mynumber,124));
+        System.out.println(run(Metod::revers,"124"));
+        System.out.println(one.function("asdfggfds"));
+        System.out.println(runner(Metod::revers,"Hello world"));
+
+    }
 }
--- Lambdas/General_Practice_05_04_22/Six/Metod.java
+++ Lambdas/General_Practice_05_04_22/Six/Metod.java
@@ -1,4 +1,17 @@
 package Lambdas.General_Practice_05_04_22.Six;
 
 public class Metod {
+
+    static String revers(String s){
+        String res = "";
+
+        for (int i = s.length()-1;i >=0;i--){
+
+            res+=s.charAt(i);
+        }
+        return res;
+    }
+    static Integer Mynumber(Integer num){
+        return (num * 888) / 123;
+    }
 }
