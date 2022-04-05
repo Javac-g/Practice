d51cd2e8b7fc281980aff7bb3a6049d30b3e64a7
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-05 22:59:30 +0300

    seven

--- Lambdas/General_Practice_05_04_22/Five/Main.java
+++ Lambdas/General_Practice_05_04_22/Five/Main.java
@@ -1,4 +1,11 @@
 package Lambdas.General_Practice_05_04_22.Five;
 
 public class Main {
+    public static void main(String...args){
+
+        Tools tools = (msg) -> msg.toUpperCase();
+        System.out.println(tools.message("string in upper case"));
+
+
+    }
 }
