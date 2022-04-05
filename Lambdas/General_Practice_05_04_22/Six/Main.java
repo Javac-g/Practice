8cdcbf62e6adf6408c58fbc02d948a0939e6303f
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-05 23:17:02 +0300

    Train

---
+++ Lambdas/General_Practice_05_04_22/Six/Main.java
@@ -0,0 +1,4 @@
+package Lambdas.General_Practice_05_04_22.Six;
+
+public class Main {
+}
---
+++ Lambdas/General_Practice_05_04_22/Six/Stool.java
@@ -0,0 +1,5 @@
+package Lambdas.General_Practice_05_04_22.Six;
+
+public interface Stool<T> {
+    T function(T t);
+}
