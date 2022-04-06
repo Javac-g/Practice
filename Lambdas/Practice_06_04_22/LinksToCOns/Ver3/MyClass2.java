3e7b45250aacfe9f9a2359d2d06d850d95a0a955
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-06 20:04:02 +0300

    five

---
+++ Lambdas/Practice_06_04_22/LinksToCOns/Ver3/MyClass2.java
@@ -0,0 +1,17 @@
+package Lambdas.Practice_06_04_22.LinksToCOns.Ver3;
+
+public class MyClass2 {
+    String str;
+
+    public MyClass2(String str) {
+        this.str = str;
+    }
+
+    public MyClass2() {
+        str = "";
+    }
+
+    String getVal(){
+        return str;
+    }
+}
