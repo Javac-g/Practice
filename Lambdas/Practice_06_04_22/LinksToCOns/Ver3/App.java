a6e517a51d27bde6d4695c1dd3d3072f374c4441
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-06 20:13:03 +0300

    done

--- Lambdas/Practice_06_04_22/LinksToCOns/Ver3/App.java
+++ Lambdas/Practice_06_04_22/LinksToCOns/Ver3/App.java
@@ -1,4 +1,15 @@
 package Lambdas.Practice_06_04_22.LinksToCOns.Ver3;
 
+import Lambdas.practice_04_04_22.Variable_Capch.MyFunc;
+
 public class App {
+    static <R,T> R myClassFactory(Tool<R,T> tool, T t){
+        return tool.function(t);
+    }
+    public static void main(String...args){
+
+        Tool<MyClass<Double>,Double> myclass = MyClass<Double>::new;
+        MyClass<Double> mc  = myClassFactory(myclass,100.1);
+        System.out.println(mc.getVal());
+    }
 }
