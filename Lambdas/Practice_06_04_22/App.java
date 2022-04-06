8381767ece97cc67ed003d837fc661c29939e5be
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-06 16:49:15 +0300

    Second

--- Lambdas/Practice_06_04_22/App.java
+++ Lambdas/Practice_06_04_22/App.java
@@ -1,4 +1,24 @@
 package Lambdas.Practice_06_04_22;
 
 public class App {
+    public static void main(String...args){
+        //One liner
+        Tool one = msg -> msg.toLowerCase();
+
+        //Block
+        Tool two = msg ->{
+            String  res = "";
+            for(int i = msg.length(); i >= 0 ; i--){
+                res+= msg.charAt(i);
+            }
+            return res;
+        };
+
+        //Generic lambdas
+
+
+
+        System.out.println(one.function("HAHAHAHAHAHHAHAHA"));
+
+    }
 }
---
+++ Lambdas/Practice_06_04_22/Tool_G.java
@@ -0,0 +1,6 @@
+package Lambdas.Practice_06_04_22;
+
+public interface Tool_G<T> {
+
+    T Function (T t);
+}
