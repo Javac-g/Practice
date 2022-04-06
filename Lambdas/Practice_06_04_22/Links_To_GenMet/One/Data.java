c722fc5f0062d965d52150bf0e8072edfe043200
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-06 19:13:10 +0300

    practice 06_04_22

---
+++ Lambdas/Practice_06_04_22/Links_To_GenMet/One/Data.java
@@ -0,0 +1,13 @@
+package Lambdas.Practice_06_04_22.Links_To_GenMet.One;
+
+public class Data {
+    private int val;
+
+    Data(int val){
+        this.val = val;
+    }
+
+    int getVal(){
+        return val;
+    }
+}
---
+++ Lambdas/Practice_06_04_22/Links_To_GenMet/One/Tool.java
@@ -0,0 +1,4 @@
+package Lambdas.Practice_06_04_22.Links_To_GenMet.One;
+
+public class Tool {
+}
