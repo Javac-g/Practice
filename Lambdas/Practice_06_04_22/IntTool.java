861691123fdf86df11b6a488c4d9d71ecc187c5a
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-06 18:03:42 +0300

    practice 06_04_22

---
+++ Lambdas/Practice_06_04_22/IntTool.java
@@ -0,0 +1,5 @@
+package Lambdas.Practice_06_04_22;
+
+public interface IntTool<T> {
+    int function(T t);
+}
--- Lambdas/Practice_06_04_22/Metod.java
+++ Lambdas/Practice_06_04_22/Metod.java
@@ -1,4 +1,18 @@
 package Lambdas.Practice_06_04_22;
 
 public class Metod {
+    static String Buff(String msg){
+        return "Static metod reached: " + msg ;
+    }
+
+    String Duff(String msg){
+        return "Exemplar metod reached:  " + msg;
+    }
+
+    static <T> String setters(T  t){
+
+        return "Value: " + t;
+
+    }
+
 }
