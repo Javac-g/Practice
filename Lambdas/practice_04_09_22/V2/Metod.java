bd107d85ba9798d3e9d21efc8d8267bf4471039d
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-09 22:26:12 +0300

    Link Metod  done

--- Lambdas/practice_04_09_22/V2/Metod.java
+++ Lambdas/practice_04_09_22/V2/Metod.java
@@ -1,4 +1,28 @@
 package Lambdas.practice_04_09_22.V2;
 
 public class Metod {
+
+
+    static String One(String msg){
+
+        return msg.toLowerCase();
+
+    }
+
+    String Two (String msg){
+
+        return msg.toUpperCase();
+
+    }
+
+    static <T> String Three(T[] val,T t){
+        int count = 0;
+
+        for (int i = 0; i < val.length; i++){
+
+            if (val[i]== t)count++;
+
+        }
+        return t + ": appears : " + count;
+    }
 }
