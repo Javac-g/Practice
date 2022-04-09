e5f7cc9073d940444d1a44d2bd837c69fea3bc5e
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-09 21:57:22 +0300

    XYZT

--- Lambdas/practice_04_09_22/XYZ.java
+++ Lambdas/practice_04_09_22/XYZ.java
@@ -9,4 +9,5 @@ public class XYZ extends XY {
         this.z = z;
 
     }
+
 }
---
+++ Lambdas/practice_04_09_22/XYZT.java
@@ -0,0 +1,14 @@
+package Lambdas.practice_04_09_22;
+
+public class XYZT extends XYZ {
+
+    int t;
+
+    XYZT(int x, int y, int z, int t){
+
+        super(x,y,z);
+
+        this.t = t;
+
+    }
+}
