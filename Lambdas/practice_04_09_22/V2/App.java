3ff326fb426321416d55d8f4ca56ccf9f48cc692
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-09 22:29:21 +0300

    New interface

--- Lambdas/practice_04_09_22/V2/App.java
+++ Lambdas/practice_04_09_22/V2/App.java
@@ -3,5 +3,8 @@ package Lambdas.practice_04_09_22.V2;
 public class App {
     public static void main(String...args){
 
+        Setup one  = msg->msg.
+
+
     }
 }
---
+++ Lambdas/practice_04_09_22/V2/Toolz.java
@@ -0,0 +1,7 @@
+package Lambdas.practice_04_09_22.V2;
+
+public interface Toolz {
+
+    String function(String msg);
+
+}
