0926e2a8818fcfeff768d35723f76988775de18a
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-09 22:35:54 +0300

    App

--- Lambdas/practice_04_09_22/V2/App.java
+++ Lambdas/practice_04_09_22/V2/App.java
@@ -1,9 +1,24 @@
 package Lambdas.practice_04_09_22.V2;
 
 public class App {
+    static String Run(Toolz toolz){
+
+    }
     public static void main(String...args){
 
-        Setup one  = msg->msg.
+        Toolz one  = msg->msg.toUpperCase();
+        System.out.println(one.function("FirsT"));
+
+        Toolz two = msg -> {
+            String s = "";
+            for (int i  = msg.length()-1; i >= 0 ; i --){
+                s += msg.charAt(i);
+            }
+            return s;
+        };
+
+        System.out.println(two.function("SeconD"));
+
 
 
     }
--- Lambdas/practice_04_09_22/V2/Toolz.java
+++ Lambdas/practice_04_09_22/V2/Toolz.java
@@ -4,4 +4,5 @@ public interface Toolz {
 
     String function(String msg);
 
+
 }
