aafaf8491e299598b1c10c943c02520f59f2d3bf
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-10 12:44:44 +0300

    Links to generic method

--- Lambdas/practice_04_10_22/App.java
+++ Lambdas/practice_04_10_22/App.java
@@ -4,7 +4,7 @@ public class App {
 
     static <T> String SR(StrInf<T> tool,T[] vals,T t){
 
-        return "Values as String : " + tool.function(vals,t);
+        return "Values as String: " + tool.function(vals,t);
 
     }
 
@@ -13,6 +13,13 @@ public class App {
         return tool.function(vals,t);
 
     }
+
+    static <T,V> T AR(allIntf<T,V> tool,V[] val,V v){
+
+        return tool.function(val,v);
+
+    }
+
     public static void main(String...args){
         String[] str = {"one","two","one","one","two","one"};
         Integer[] num = {1,1,1,1,12,2,2,3,4,2,2,1,1,2,3,4,5,6,7,8,9};
@@ -23,6 +30,11 @@ public class App {
 
         System.out.println("Values as Integer: " + IR(Metod::intOne,str,"one"));
         System.out.println("Values as Integer: " + IR(Metod::intOne,num,1));
+
+        System.out.println("Values as Generic Metod:" + AR(Metod::intOne,num,1));
+        System.out.println("Values as Generic Metod:" + AR(Metod::strOne,num,1));
+        System.out.println("Values as Generic Metod:" + AR(Metod::intOne,str,"one"));
+        System.out.println("Values as Generic Metod:" + AR(Metod::strOne,str,"one"));
 
 
     }
---
+++ Lambdas/practice_04_10_22/allIntf.java
@@ -0,0 +1,5 @@
+package Lambdas.practice_04_10_22;
+
+public interface allIntf<T,V> {
+    T function(V[] val,V v);
+}
