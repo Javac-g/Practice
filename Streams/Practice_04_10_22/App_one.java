0eecd7b994b62f532b6fe82a9e0043456df76d6d
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-10 21:49:07 +0300

    fixed read(gap  must be first)

--- Streams/Practice_04_10_22/App_one.java
+++ Streams/Practice_04_10_22/App_one.java
@@ -5,24 +5,25 @@ import java.io.*;
 public class App_one {
     public static void main(String...args) throws IOException {
 
+        int i = 0;
 
+        if(args.length != 1){
+
+            System.out.println("File not found");
+            return;
+
+        }
 
 
         try(FileInputStream fileInputStream = new FileInputStream(args[0])){
-                int i = 0;
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            PrintWriter pr = new PrintWriter(System.out,true);
-                if(args.length != 1){
 
-                        System.out.println("File not found");
-
-                }
                 do{
                     i = fileInputStream.read();
-                    if(i != -1){
-                        System.out.print( (char)i + " ");
-                    }
+
+                    if(i != -1)System.out.print( (char)i);
+
                 }while (i != -1);
+
         } catch (IOException e) {
             e.printStackTrace();
 
